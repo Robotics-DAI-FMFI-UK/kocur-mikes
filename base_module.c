1ff4bd92c710d8d153aa5e1fcf7806a4c84ad026
pavelpetrovic <pavel.petrovic@gmail.com>
2016-11-01 14:41:41 +0100

    propagating protocol improvements to this layer

--- base_module.c
+++ base_module.c
@@ -12,8 +12,11 @@
 #include <signal.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define MAX_PACKET_LENGTH 200
+
+static int current_azimuth;
 
 static pthread_mutex_t base_module_lock;
 
@@ -22,49 +25,64 @@ static int fdW[2];
 
 static pid_t plink_child;
 
+static unsigned char new_base_data_arrived;
+static unsigned char base_initialized;
+
 void connect_base_module()
 {
-        if (pipe(fdR) < 0)
-        {
-            perror("pipe2()");
-            exit(-1);
-        }
-        if (!pipe(fdW) < 0)
-        {
-            perror("pipe2()");
-            exit(-1);
-        }
+    if (pipe(fdR) < 0)
+    {
+        mikes_log(ML_ERR, "base: pipe2()");	
+        base_initialized = 0;
+        return;
+    }
+    if (!pipe(fdW) < 0)
+    {
+        mikes_log(ML_ERR, "base: pipe2()");	
+        base_initialized = 0;
+        return;
+    }
 
-        if ((plink_child = fork()) == 0)
+    if ((plink_child = fork()) == 0)
+    {
+        /* child */
+
+        close(0);
+        close(1);
+        dup2(fdR[0], 0);
+        dup2(fdW[1], 1);
+        close(fdR[0]);
+        close(fdR[1]);
+        close(fdW[0]);
+        close(fdW[1]);
+
+        if (execl("/usr/bin/plink", "/usr/bin/plink", "/dev/ttyUSB0",
+                  "-serial", "-sercfg", "115200,N,n,8,1", NULL) < 0)
         {
-            /* child */
-
-            close(0);
-            close(1);
-            dup2(fdR[0], 0);
-            dup2(fdW[1], 1);
-            close(fdR[0]);
-            close(fdR[1]);
-            close(fdW[0]);
-            close(fdW[1]);
-
-            if (execl("/usr/bin/plink", "/usr/bin/plink", "/dev/ttyUSB0",
-             "-serial", "-sercfg", "115200,N,n,8,1", NULL) < 0)
-            {
-                perror("child execl()");
-                exit(-1);
-            }
+            mikes_log(ML_ERR, "base: child execl()");
+            base_initialized = 0;
+            return;
         }
+    }
 
     if (plink_child < 0)
     {
-            perror("fork()");
-            exit(-1);
+        mikes_log(ML_ERR, "base: child execl()");
+        base_initialized = 0;
+        return;
     }
 
     close(fdR[0]);
     close(fdW[1]);
+    if (fcntl( fdW[0], F_SETFL, fcntl(fdW[0], F_GETFL) | O_NONBLOCK) < 0)
+    {
+        mikes_log(ML_ERR, "base: setting nonblock on read pipe end");
+        base_initialized = 0;
+    }
+
     mikes_log(ML_INFO, "base module connected");
+    current_azimuth = NO_AZIMUTH;
+    base_initialized = 1;
 }
 
 base_data_type local_data;
@@ -77,22 +95,51 @@ void read_base_packet()
     do {
         if ((numRead = read(fdW[0], &ch, 1)) < 0)
         {
-            perror("read()");
-            exit(-1);
+            if (errno != EAGAIN)
+            {
+                perror("read()");
+                exit(-1);
+            }
         }
     } while (program_runs && (ch != '$'));
 
+    unsigned char more_packets_in_queue = 0;
     char line[1024];
-    int lnptr = 0;
     do {
-      if ((numRead = read(fdW[0], line + lnptr, 1)) < 0)
-      {
-         perror("read()");
-         exit(-1);
-      }
-      lnptr += numRead;
-      if (lnptr > 1023) break;
-    } while (program_runs && (line[lnptr - 1] != '\n'));
+        int lnptr = 0;
+        do {
+          if ((numRead = read(fdW[0], line + lnptr, 1)) < 0)
+          {
+              if (errno != EAGAIN)
+              {
+                  perror("read()");
+                  exit(-1);
+              }
+          }
+          lnptr += numRead;
+          if (lnptr > 1023) break;
+        } while (program_runs && (line[lnptr - 1] != '\n'));
+
+        more_packets_in_queue = 0;
+        while (program_runs)
+        {
+            if (read(fdW[0], &ch, 1) < 0)
+            {
+                if (errno == EAGAIN) break;
+                else
+                {
+                    perror("read()");
+                    exit(-1);
+                }
+            }
+            if (ch == '$')
+            {
+                more_packets_in_queue = 1;
+                break;
+            }
+        }
+    } while (program_runs && more_packets_in_queue);
+
     pthread_mutex_lock(&base_module_lock);
     sscanf(line, "%ld%ld%hd%hd%hd%hd%hd%hd%hd%hd%hd%hd%hd%hd%hd",
                                   &(local_data.counterA), &(local_data.counterB), &(local_data.velocityA),
@@ -100,8 +147,16 @@ void read_base_packet()
                                   &(local_data.dist3), &(local_data.cube), &(local_data.heading),
                                   &(local_data.ax), &(local_data.ay), &(local_data.az),
                                   &(local_data.gx), &(local_data.gy), &(local_data.gz));
+    new_base_data_arrived = 1;
+    //printf("CA: %ld,  CB: %ld\n", local_data.counterA, local_data.counterB);
     pthread_mutex_unlock(&base_module_lock);
     //mikes_log(ML_INFO, line);
+}
+
+void wait_for_new_base_data()
+{
+    new_base_data_arrived = 0;
+    while (!new_base_data_arrived) usleep(1000);
 }
 
 void set_motor_speeds(int left_motor, int right_motor)
@@ -129,17 +184,24 @@ void stop_now()
        perror("mikes:base");
        mikes_log(ML_ERR, "base: could not send command");
     }
+    current_azimuth = NO_AZIMUTH;
 }
 
 void follow_azimuth(int azimuth)
 {
     char cmd[20];
+    current_azimuth = azimuth;
     sprintf(cmd, "@A%d%d%d", azimuth / 100, (azimuth % 100) / 10, azimuth % 10);
     if (write(fdR[1], cmd, strlen(cmd)) < strlen(cmd))
     {
       perror("mikes:base");
       mikes_log(ML_ERR, "base: could not send azimuth");
     }
+}
+
+int get_current_azimuth()
+{
+    return current_azimuth;
 }
 
 void reset_counters()
@@ -153,7 +215,7 @@ void reset_counters()
 
 void regulated_speed(int left_motor, int right_motor)
 {
-   char cmd[40];
+    char cmd[40];
     int lm = left_motor;
     int rm = right_motor;
 
@@ -170,6 +232,7 @@ void regulated_speed(int left_motor, int right_motor)
       perror("mikes:base");
       mikes_log(ML_ERR, "base: could not send regulated speed");
     }
+    current_azimuth = NO_AZIMUTH;
 }
 
 void *base_module_thread(void *args)
@@ -191,12 +254,15 @@ void *base_module_thread(void *args)
 void init_base_module()
 {
     pthread_t t;
+    base_initialized = 0;
     connect_base_module();
+    if (!base_initialized) return;
     pthread_mutex_init(&base_module_lock, 0);
     if (pthread_create(&t, 0, base_module_thread, 0) != 0)
     {
       perror("mikes:base");
       mikes_log(ML_ERR, "creating thread for base module");
+      base_initialized = 0;
     }
     else threads_running_add(1);
 }
@@ -217,3 +283,41 @@ short angle_difference(short alpha, short beta)
   return diff;
 }
 
+void cancel_azimuth_mode()
+{
+    if (write(fdR[1], "@X", 2) < 2)
+    {
+      perror("mikes:base");
+      mikes_log(ML_ERR, "base: could not cancel azimuth mode");
+    }
+    current_azimuth = NO_AZIMUTH;
+}
+
+void pause_status_reporting()
+{
+    if (write(fdR[1], "@-", 2) < 2)
+    {
+      perror("mikes:base");
+      mikes_log(ML_ERR, "base: could not pause reporting");
+    }
+}
+
+void resume_status_reporting()
+{
+    if (write(fdR[1], "@+", 2) < 2)
+    {
+      perror("mikes:base");
+      mikes_log(ML_ERR, "base: could not resume reporting");
+    }
+}
+
+void set_laziness(unsigned char laziness)
+{
+    char cmd[10];
+    sprintf(cmd, "@L %d%d", laziness / 10, laziness % 10);
+    if (write(fdR[1], cmd, strlen(cmd)) < strlen(cmd))
+    {
+      perror("mikes:base");
+      mikes_log(ML_ERR, "base: could not set laziness");
+    }
+}
--- base_module.h
+++ base_module.h
@@ -2,24 +2,83 @@
 #define _BASE_MODULE_H
 
 #include <pthread.h>
+#include <math.h>
 
+#define WHEEL_DIAMETER_IN_MM          157
+#define WHEEL_PERIMETER_IN_MM         (int)(WHEEL_DIAMETER_IN_MM * M_PI)
+#define COUNTER_TICKS_PER_REVOLUTION  144
+
+#define NO_AZIMUTH 400
+
+// convert counter ticks to mm
+#define COUNTER2MM(COUNTER) (COUNTER * WHEEL_PERIMETER_IN_MM / COUNTER_TICKS_PER_REVOLUTION)
+
+// convert mm to counter ticks
+#define MM2COUNTER(MM) (MM * COUNTER_TICKS_PER_REVOLUTION / WHEEL_PERIMETER_IN_MM)
 
 typedef struct astruct {
+  // 144 ticks per revolution A=left, B=right
   long counterA, counterB;
-  short velocityA, velocityB; 
-  short dist1, dist2, dist3, cube, heading, ax, ay, az, gx, gy, gz;
+  // current angular velocity in deg/s
+  short velocityA, velocityB;
+  // IR distance sensors
+  short dist1, dist2, dist3;
+  // cube presence IR sensor
+  short cube;
+  // last compass reading 0-360
+  short heading;
+  // normalized accelerometer (ms^-2)
+  short ax, ay, az;
+  // normalized gyroscope
+  short gx, gy, gz;
 } base_data_type;
 
+// open serial communication with arduino base board
 void init_base_module();
+
+// retrieve the latest sensor data from base board
 void get_base_data(base_data_type *buffer);
 
+// waits until the next sensor data from the base board have not been received
+// since the serial line is full duplex, one packet could have been already on the way,
+// therefore you need to call this twice, to be sure the next status reflects the last
+// output packet
+void wait_for_new_base_data();
+
+// direct motor power control mode: start moving with the specified speeds
 void set_motor_speeds(int left_motor, int right_motor);
+
+// stop moving in every mode, also cancels azimuth and velocity modes
 void stop_now();
+
+// azimuth follow mode: start moving towards the specified azimuth (0-360), use the last specified speed of direct motor power control mode
 void follow_azimuth(int azimuth);
+
+// reset counterA, counterB counters in the base board at this moment
 void reset_counters();
 
+// velocity control mode: start moving so that the
 void regulated_speed(int left_motor, int right_motor);
 
+// cancel azimuth mode
+void cancel_azimuth_mode();
+
+// pause status reporting (sensory data will not be updated)
+void pause_status_reporting();
+
+// resume status reporting (start updating sensory data again)
+void resume_status_reporting();
+
+// configure laziness for velocity regulation mode
+void set_laziness(unsigned char laziness);
+
+// utility function to compute directional angle from alpha to beta (+/- 180), all values in deg.
 short angle_difference(short alpha, short beta);
+
+// utility function to convert the angular counter to travelled distance in mm
+short counter2mm(short counter);
+
+// utility function to return the current followed azimuth or NO_AZIMUTH, if none
+int get_current_azimuth();
 
 #endif
