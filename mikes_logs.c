689b5d279c3887e83516a930411527348ff31d1e
pavelpetrovic <pavel.petrovic@gmail.com>
2016-11-12 21:58:30 +0100

    print time to logfile

--- mikes_logs.c
+++ mikes_logs.c
@@ -4,31 +4,46 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "mikes_logs.h"
 #include "config_mikes.h"
 
 static char *log_filename;
+static long start_time;
 
 void init_mikes_logs()
 {
+  struct timeval tv;
   char *filename_str = "/usr/local/logs/mikes/%ld_%s";
   char *lastlog = "/usr/local/logs/mikes/last";
   char *filename_base = "mikes.log";
+
+  gettimeofday(&tv, 0);
+  start_time = 100 * tv.tv_sec + (tv.tv_usec / 10000L); 
+
   log_filename = (char *)malloc(strlen(filename_str) + 20 + strlen(filename_base));
   if (log_filename == 0)
   {
     perror("mikes:logs");
     exit(1);
   }
-  time_t t;
-  time(&t);
-  sprintf(log_filename, filename_str, t, filename_base);
+
+  time_t tm;
+  time(&tm);
+  sprintf(log_filename, filename_str, tm, filename_base);
+
   FILE *f = fopen(log_filename, "w+");
   fclose(f);
+
   unlink(lastlog);
   symlink(log_filename, lastlog);
+
   if (mikes_config.print_all_logs_to_console) mikes_log(ML_INFO, "printing all logs to console");
+  char ctm[40];
+  sprintf(ctm, "%s", ctime(&tm));
+  ctm[strlen(ctm) - 1] = 0;
+  mikes_log(ML_INFO, ctm);
 }
 
 static char *log_type_str[4] = { "INFO", "WARN", " ERR", "DEBG" };
@@ -49,13 +64,21 @@ FILE *try_opening_log(unsigned int log_type)
   return f;
 }
 
+long get_run_time()
+{
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return (100 * tv.tv_sec + (tv.tv_usec / 10000L)) - start_time;
+}
+
 void mikes_log(unsigned int log_type, char *log_msg)
 {
+  long run_time = get_run_time();
 
   FILE *f = try_opening_log(log_type);
   if (f)
   {
-      fprintf(f, "%s: %s\n", log_type_str[log_type], log_msg);
+      fprintf(f, "%05ld.%02d %s: %s\n", run_time / 100L, (int)(run_time % 100), log_type_str[log_type], log_msg);
       fclose(f);
   }
 
@@ -65,11 +88,12 @@ void mikes_log(unsigned int log_type, char *log_msg)
 
 void mikes_log_val2(unsigned int log_type, char *log_msg, int val, int val2)
 {
+  long run_time = get_run_time();
 
   FILE *f = try_opening_log(log_type);
   if (f)
   {
-      fprintf(f, "%s: %s %d %d\n", log_type_str[log_type], log_msg, val, val2);
+      fprintf(f, "%05ld.%02d %s: %s %d %d\n", run_time / 100L, (int)(run_time % 100), log_type_str[log_type], log_msg, val, val2);
       fclose(f);
   }
 
@@ -79,14 +103,16 @@ void mikes_log_val2(unsigned int log_type, char *log_msg, int val, int val2)
 
 void mikes_log_val(unsigned int log_type, char *log_msg, int val)
 {
+  long run_time = get_run_time();
 
   FILE *f = try_opening_log(log_type);
   if (f)
   {
-      fprintf(f, "%s: %s %d\n", log_type_str[log_type], log_msg, val);
+      fprintf(f, "%05ld.%02d %s: %s %d\n", run_time / 100L, (int)(run_time % 100), log_type_str[log_type], log_msg, val);
       fclose(f);
   }
 
   if (mikes_config.print_all_logs_to_console)
     printf("%s: %s %d\n", log_type_str[log_type], log_msg, val);
 }
+
