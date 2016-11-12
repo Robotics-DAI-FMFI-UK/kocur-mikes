bd70cf696069192ded420e2afc527538e2e6f14b
pavelpetrovic <pavel.petrovic@gmail.com>
2016-11-12 17:31:26 +0100

    distance utility fn

--- util.c
+++ util.c
@@ -1,5 +1,10 @@
 #include <sys/time.h>
 
+double distance(double x1, double y1, double x2, double y2)
+{
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
 long usec()
 {
   struct timeval tv;
--- util.h
+++ util.h
@@ -1,6 +1,9 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+// square of distance of two points
+double distance(double x1, double y1, double x2, double y2);
+
 // return current time in usec (up to 71 minutes)
 long usec();
 
